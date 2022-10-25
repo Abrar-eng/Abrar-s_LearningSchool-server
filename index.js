3597f0270f0c258d223e9a857b38924db69555eb
Abrar-eng <abrarulhasan600@gmail.com>
2022-10-26 02:03:05 +0600

    new add

--- index.js
+++ index.js
@@ -16,16 +16,7 @@ app.get('/news-categories', (req, res) => {
     res.send(categories)
 });
 
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    if (id === '08') {
-        res.send(news);
-    }
-    else {
-        const category_news = news.filter(n => n.category_id === id);
-        res.send(category_news);
-    }
-})
+
 
 app.get('/news', (req, res) =>{
     res.send(news);
